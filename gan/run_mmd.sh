e5dfdd545055f3bbc950163ab370d11fd7beac55
Mikolaj Binkowski <mikbinkowski@gmail.com>
2017-10-24 16:14:31 +0100

    .

--- gan/run_mmd.sh
+++ gan/run_mmd.sh
@@ -9,8 +9,17 @@ fi
 
 CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0} python main_mmd.py \
     --model=mmd \
+    --suffix= \
     --use_kernel --is_train=True \
+    --threads=2 \
     --name=mmd \
-    --dataset=lsun \
-    --max_iteration=5000 --init=0.1 --learning_rate=.00011 --batch_size=128 \
-    --dataset=mnist --architecture=dc --dc_discriminator --kernel=rbf \
+    --max_iteration=50000 --init=0.1 \
+    --learning_rate=.000104 --batch_size=128 --real_batch_size=128 \
+    --architecture=dcgan --dc_discriminator \
+    --kernel=distance \
+    --dsteps=5 --start_dsteps=5 \
+    --batch_norm \
+    --dataset=mnist \
+    --gradient_penalty=10.0 --output_size=64 \
+    --gf_dim=32 --df_dim=32 --dof_dim=16 \
+    --log=False
--- gan/run_tmmd.sh
+++ gan/run_tmmd.sh
@@ -7,17 +7,18 @@ else
     is_train=True
 fi
 
-python main_mmd.py \
+CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0} python main_mmd.py \
     --model=tmmd \
+    --suffix= \
     --use_kernel --is_train=True \
-    --name=tmmd\
-    --max_iteration=20000 --init=0.1 --learning_rate=.00098 --batch_size=128 \
-    --dataset=cifar10 --architecture=dc --dc_discriminator --kernel=rq \
-##    --gradient_penalty=10.0
-
-##python main_mmd.py \
-##    --model=tmmd \
-##    --use_kernel --is_train=True \
-##    --name=tmmd\
-##    --max_iteration=20000 --init=0.1 --learning_rate=.00095 --batch_size=512 \
-##    --dataset=cifar10 --architecture=mlp --dc_discriminator --kernel=rq \
+    --threads=2 \
+    --name=temp \
+    --max_iteration=5000 --init=0.1 \
+    --learning_rate=.000140 --batch_size=128 \
+    --architecture=dfc --dc_discriminator \
+    --kernel=mix_rq \
+    --dsteps=5 --start_dsteps=5 \
+    --batch_norm \
+    --dataset=cifar10 \
+    --gradient_penalty=10.0 \
+    --log=False
